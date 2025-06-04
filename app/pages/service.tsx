1327882ab26a10eae3990f8c59e30d03f34a4659
Henok <henokaragaw17@gmail.com>
2025-06-04 04:01:26 -0900

    setting up route pages

---
+++ app/pages/service.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div>Welcome to service page</div>
+  )
+}
+
+export default Service
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import HeroPage from "components/Hero";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -8,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <>Hey</>;
+  return (
+    <>
+    <HeroPage />
+    </>
+  );
 }
